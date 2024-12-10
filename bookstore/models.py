cb368597e323568ef6fe8a0598f463b4826587c6
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-12-10 14:06:13 +0000

    add models

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,4 +1,5 @@
 from cloudinary.models import CloudinaryField
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -17,8 +18,8 @@ class Book(models.Model):
     condition = models.CharField(max_length=10, choices=CONDITION_CHOICES, null=True, blank=True)
     rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    cover_image = CloudinaryField('image')
-    created_at = models.DateTimeField(auto_now_add=True)
+    cover_image = CloudinaryField('image', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
     
     def __str__(self):
         return self.title
