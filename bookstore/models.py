a0a3d60d8b77bd512b9a67085fc327f47e37e0b4
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-12-12 14:19:21 +0000

    comment out search bar until I can fix, add load static to try and fix logo img not serving

--- bookstore/models.py
+++ bookstore/models.py
@@ -6,19 +6,19 @@ from django.db import models
 
 class Book(models.Model):
     CONDITION_CHOICES = [
-        ('new', 'New'),
-        ('used', 'Used'),
+        ('like-new', 'Like-New'),
+        ('good', 'Good'),
+        ('fair', 'Fair'),
     ]
     
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255, null=True, blank=True)
-    category = models.CharField(max_length=100, null=True, blank=True)
     date_published = models.DateField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     condition = models.CharField(max_length=10, choices=CONDITION_CHOICES, null=True, blank=True)
-    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    cover_image = CloudinaryField('image', null=True, blank=True)
+    cover_image = CloudinaryField('image', default='placeholder')
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     
     def __str__(self):
