c09dd930ed30dc4aa9e2641868d930773cd25999
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-12-13 14:58:54 +0000

    add add to basket and remove basket views, urls. BUG: not adding items to basket, but redirecting to basket page

--- basket/urls.py
+++ basket/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
+    path('add/<int:book_id>/', views.add_to_basket, name='add_to_basket'),
+    path('remove/<int:book_id>/', views.remove_from_basket, name='remove_from_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from bookstore.models import Book
 
 # Create your views here.
 
 def view_basket(request):
     """ A view that renders the basket contents page """
 
-    return render(request, 'basket/basket.html')
+    basket = request.session.get('basket', {})
+    basket_items = []
+
+    for book_id, quantity in basket.items():
+        book = get_object_or_404(Book, id=book_id)
+        basket_items.append({
+            'book': book,
+            'quantity': quantity,
+        })
+
+    return render(request, 'basket/basket.html', {'basket_items': basket_items})
+
+
+def add_to_basket(request, book_id):
+    """
+    """
+
+    book = get_object_or_404(Book, id=book_id)
+    if 'basket' not in request.session:
+        request.session['basket'] = {}
+    basket = request.session.get('basket', {})
+    basket[str(book.id)] = basket.get(str(book.id), 0) + 1
+    request.session['basket'] = basket
+    return redirect('view_basket')
+
+def remove_from_basket(request, book_id):
+    """
+    """
+
+    basket = request.session.get('basket', {})
+    if str(book_id) in basket:
+        del basket[str(book_id)]
+    request.session['basket'] = basket
+    return redirect('view_basket')
