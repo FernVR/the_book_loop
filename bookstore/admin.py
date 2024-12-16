ae64371120ef1ddb402aec8b199968ac247bf625
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-12-16 12:13:28 +0000

    add stock model, admin and buttons to template - out of stock items displayed to user

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -5,6 +5,10 @@ from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at') 
+    list_display = ('title', 'created_at', 'stock') 
     search_fields = ('title',)  
     ordering = ('-created_at',)
+    list_editable = ('stock',)
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
--- bookstore/models.py
+++ bookstore/models.py
@@ -24,8 +24,14 @@ class Book(models.Model):
     rating = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     cover_image = CloudinaryField('image', default='placeholder')
+    stock = models.PositiveIntegerField(default=0)
+    in_stock = models.BooleanField(default=False, editable=False)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     
+    def save(self, *args, **kwargs):
+        self.in_stock = self.stock > 0
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
 
