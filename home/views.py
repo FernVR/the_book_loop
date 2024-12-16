2329574078690239b1a00a65a86cfe7766a83169
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-12-16 15:23:53 +0000

    add 404 page and custom error handling

--- home/views.py
+++ home/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_view(request):
+    """
+    Renders Home Page. 
+    """
     return render(request, 'home/index.html')
+
+
+def custom_404(request, exception):
+    """
+    Renders 404 page.
+    """
+    return render(request, '404.html', status=404)
--- the_book_loop/urls.py
+++ the_book_loop/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404
+from home.views import custom_404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,5 @@ urlpatterns = [
     path('bookstore/', include('bookstore.urls', namespace='bookstore')),
     path('basket/', include('basket.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = custom_404
