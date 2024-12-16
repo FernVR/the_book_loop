d5e7c2199096d5d15525b963931f0b828bddc693
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-12-16 13:59:20 +0000

    fixed: add basket functionality, using contexts.py

---
+++ basket/contexts.py
@@ -0,0 +1,44 @@
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from bookstore.models import Book
+
+
+def basket_contents(request):
+    """ 
+    Context processor to manage basket contents.
+    """
+
+    basket_items = []
+    total = 0
+    book_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity, in basket.items():
+        book = get_object_or_404(Book, pk=item_id)
+        total += quantity * book.price
+        book_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'book': book,
+        })
+
+    delivery = Decimal(3) if total < 40 else Decimal(0)
+    grand_total = delivery + total
+
+    context = {
+        'basket_items': basket_items,
+        'total': total,
+        'book_count': book_count,
+        'delivery': delivery,
+        'free_delivery_threshold': 40,
+        'grand_total': grand_total,
+        }
+
+    return context
+
+
+
+
+
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from bookstore.models import Book
 
 # Create your views here.
@@ -6,37 +7,35 @@ from bookstore.models import Book
 def view_basket(request):
     """ A view that renders the basket contents page """
 
-    basket = request.session.get('basket', {})
-    basket_items = []
 
-    for book_id, quantity in basket.items():
-        book = get_object_or_404(Book, id=book_id)
-        basket_items.append({
-            'book': book,
-            'quantity': quantity,
-        })
-
-    return render(request, 'basket/basket.html', {'basket_items': basket_items})
+    return render(request, 'basket/basket.html')
 
 
 def add_to_basket(request, book_id):
     """
+    Add a book to the basket.
     """
 
-    book = get_object_or_404(Book, id=book_id)
-    if 'basket' not in request.session:
-        request.session['basket'] = {}
+    book = get_object_or_404(Book, pk=item_id)
     basket = request.session.get('basket', {})
-    basket[str(book.id)] = basket.get(str(book.id), 0) + 1
+
+    if item_id in basket:
+        messages.error(request, "This book is already in your basket.")
+    else:
+        basket[item_id] = 1
+        messages.success(request, f'Added "{book.title}" to your basket.')
+
     request.session['basket'] = basket
-    return redirect('view_basket')
+    return redirect('book_detail', item_id=item_id)
+
+
 
 def remove_from_basket(request, book_id):
     """
+    Remove an item from the basket.
     """
 
-    basket = request.session.get('basket', {})
-    if str(book_id) in basket:
-        del basket[str(book_id)]
-    request.session['basket'] = basket
-    return redirect('view_basket')
+    
+    
+
+
