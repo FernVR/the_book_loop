695d6ccde3880652eda773341606ebaad2f95d69
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-12-19 13:14:40 +0000

    add search bar functionality

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,7 +1,11 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
+from django.contrib import messages
+from django.urls import reverse
 from .models import Book, Review
 from .forms import ReviewForm
+
 
 # Create your views here.
 
@@ -11,26 +15,30 @@ def bookstore(request):
     """
 
     books = Book.objects.all().order_by('-created_at')
-    paginator = Paginator(books, 12)  # Show 12 books per page
+    query = None
+
+    if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('bookstore:bookstore'))
+            
+            queries = Q(title__icontains=query) | Q(description__icontains=query)
+            books = books.filter(queries)
+
+    paginator = Paginator(books, 12)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'bookstore/bookstore.html', {'page_obj': page_obj})
-
-
-def book_search(request):
-    """
-    """
-    query = request.GET.get('q', '')
-    if query:
-        books = Book.objects.filter(title__icontains=query)  # You can also search by other fields like author
-    else:
-        books = Book.objects.all()
-    
-    return render(request, 'bookstore/bookstore.html', {
+    context = {
         'books': books,
-        'query': query,
-    })
+        'search_term': query,
+        'page_obj': page_obj,
+    }
+
+    return render(request, 'bookstore/bookstore.html', context)
+
+
 
 
 def book_detail(request, book_id):
