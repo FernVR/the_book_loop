940f3ee3bf6e0329c1901a5cb4b6a0c577af4a05
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-12-20 12:06:28 +0000

    add create user profile functionality

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from .models import UserProfile
+from django.contrib import messages
 
 # Create your views here.
 
+@login_required
 def user_profile(request):
     """ 
     Returns rendered user profile page
-    """
 
-    return render(request, 'user_profile/user_profile.html')
+    """
+    try:
+        profile = request.user.userprofile
+    except UserProfile.DoesNotExist:
+        messages.success(request, "You don't have a profile")
+        return HttpResponseRedirect(reverse('home'))
+
+    template = 'user_profile/user_profile.html'
+    context = {
+        'profile': profile
+    }
+
+    return render(request, template, context)
