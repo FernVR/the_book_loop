1c55fb86d19bdefc90ebbefc37adf0c0e130a57e
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-12-24 11:47:07 +0000

    add stripe_pid to order model and cache checkout data url

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import checkout
+from .views import checkout, cache_checkout_data
+from .webhooks import webhook
 
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('webhook/', views.webhook, name='webhook'),
 ]
