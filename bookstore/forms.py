74912fd4b3f8219a679db828fdf3c40215799e67
Fern Van Rooyen <fern.w.1998@gmail.com>
2025-01-08 13:05:45 +0000

    add edit book and add book forms

--- bookstore/forms.py
+++ bookstore/forms.py
@@ -13,6 +13,13 @@ class BookForm(forms.ModelForm):
         model = Book
         fields = ['title', 'author', 'date_published', 'description', 'condition', 'rating', 'price', 'cover_image', 'stock']
         widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control'}),
             'date_published': forms.DateInput(attrs={'type': 'date'}),
             'description': forms.Textarea(attrs={'rows': 5}),
+            'condition': forms.Select(attrs={'class': 'form-control'}),
+            'rating': forms.NumberInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'cover_image': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+            'stock': forms.NumberInput(attrs={'class': 'form-control'}),
         }
