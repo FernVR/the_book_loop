65f5b472c5e035c181cbe628721f39dcbf46aae4
Fern Van Rooyen <fern.w.1998@gmail.com>
2025-01-08 09:45:06 +0000

    finalise design on customer service page/form

--- customer_service/forms.py
+++ customer_service/forms.py
@@ -5,3 +5,8 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = CustomerSupport
         fields = ['full_name', 'email', 'message']
+        widgets = {
+            'full_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your full name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter your message', 'rows': 4}),
+        }
