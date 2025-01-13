24223b6f9c557d4eb40ff503b3b5d8cd3f8e37a8
Fern Van Rooyen <fern.w.1998@gmail.com>
2025-01-13 08:47:00 +0000

    add stripe js and views cache checkout data

--- checkout/views.py
+++ checkout/views.py
@@ -13,25 +13,20 @@ from user_profile.models import UserProfile
 
 # Create your views here.
 
-
 @require_POST
 def cache_checkout_data(request):
-    """
-    Cache checkout data
-    """
     try:
-        pid = request.POST.get('client_secret').split('_secret')[0]
+        pid = request.POST.get('client_secret', '').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
-            'bag': json.dumps(request.session.get('bag', {})),
-            'save_info': request.POST.get('save_info'),
-            'username': request.user,
+            'basket': json.dumps(request.session.get('basket', {})),
+            'save_info': request.POST.get('save_info', False),
+            'username': str(request.user),
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot be \
-            processed right now. Please try again later.')
-        return HttpResponse(content=e, status=400)
+        return HttpResponse(content=str(e), status=400)
+
 
 
 
